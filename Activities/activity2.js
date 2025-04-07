96be49e77d669967d4b2b64c07e52f769b679f82
mrdybsn <mardybesana0705@gmail.com>
2025-04-07 15:16:48 +0800

    Activity2

---
+++ Activities/activity2.js
@@ -0,0 +1,19 @@
+function TotalAmount(totalPurchase, isMember) {
+    var discount_price, discounted_price;
+    if(totalPurchase > 100 && isMember) {
+        discount_price = totalPurchase * 0.2;
+        discounted_price = totalPurchase - discount_price;
+        return discounted_price;
+    } else if (totalPurchase > 50) {
+        discount_price = totalPurchase * 0.1;
+        discounted_price= totalPurchase - discount_price;
+        return discounted_price;
+    } else {
+        console.log(totalPurchase, "No discount");
+    }
+    return discounted_price;
+}
+
+console.log(Discount(120, 'True'));
+console.log(Discount(60, 'False'));
+console.log(Discount(30, 'False'));
